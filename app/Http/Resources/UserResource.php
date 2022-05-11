bef4559a0d8b7b0dd3e76bac6d881d3a29f81d21
Elomena Idise <menadio1@gmail.com>
2022-05-11 08:01:09 +0100

    added disk for media uploads

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,6 @@ class UserResource extends JsonResource
     {
         return [
             'id'        => $this->id,
-            'username'  => $this->username,
             'email'     => $this->email,
             'profile'   => new ProfileResource($this->whenLoaded('profile')),
         ];
