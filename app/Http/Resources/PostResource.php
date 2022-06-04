71a5fcb9301efae549774ea9d6b6a2f3d969c26e
Elomena Idise <menadio1@gmail.com>
2022-06-04 12:19:37 +0100

    added more data to post resource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,12 +15,14 @@ class PostResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'user_id' => $this->user_id,
-            'post' => $this->post,
-            'image' => $this->getFirstMediaUrl('post-images'),
-            'comments' => CommentResource::collection($this->whenLoaded('comments')),
-            'created_at' => $this->created_at,
+            'id'        => $this->id,
+            'user_id'   => $this->user_id,
+            'fullname'  => $this->user->profile->first_name . ' ' . $this->user->profile->last_name,
+            'avatar'    => $this->user->getFirstMediaUrl('user-photos'),
+            'post'      => $this->post,
+            'image'     => $this->getFirstMediaUrl('post-images'),
+            'comments'  => CommentResource::collection($this->whenLoaded('comments')),
+            'created_at'=> $this->created_at,
         ];
     }
 }
